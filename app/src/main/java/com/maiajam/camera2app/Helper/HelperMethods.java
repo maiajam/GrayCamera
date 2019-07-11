8bfd3518cf10d1d7fab57f9b6fb27129e4c94690
mai waleed <maiajam88@hotmail.com>
2019-07-11 10:12:25 +0300

    update

--- app/src/main/java/com/maiajam/camera2app/Helper/HelperMethods.java
+++ app/src/main/java/com/maiajam/camera2app/Helper/HelperMethods.java
@@ -1,12 +1,19 @@
 package com.maiajam.camera2app.Helper;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.util.Log;
 import android.util.Size;
+import android.view.SurfaceHolder;
 import android.view.View;
 
 public class HelperMethods {
@@ -22,11 +29,13 @@ public class HelperMethods {
         return contentview.getMeasuredWidth();
     }
 
-    public static Size[] getScreenSize(Context context, CameraDevice cameraDevice) {
+    public static Size[] getScreenSize(Context context) {
+
         CameraManager manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
         Size[] ScreenSizes = null;
         try {
-            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
+            String cameraId = manager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
 
             if (characteristics != null) {
                 ScreenSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
@@ -35,5 +44,38 @@ public class HelperMethods {
             e.printStackTrace();
         }
         return ScreenSizes;
+    }
+
+
+    public static void DrawFingersShapre(Context context, SurfaceHolder holder)
+    {
+        Canvas canvas = holder.lockCanvas();
+        if (canvas == null) {
+            Log.e("", "Cannot draw onto the canvas as it's null");
+        }else {
+
+            Size[] size = HelperMethods.getScreenSize(context);
+            Paint myPaint = new Paint();
+            myPaint.setColor(Color.BLACK);
+            myPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+            myPaint.setAntiAlias(true);
+            myPaint.setStrokeWidth(10);
+            myPaint.setStyle(Paint.Style.STROKE);
+            int recWidth = size[0].getWidth()/4 ;
+            int recHight = size[0].getHeight() ;
+            canvas.drawColor(Color.TRANSPARENT);
+            canvas.drawRect(20, recHight/2 +100,  recWidth+20, recHight , myPaint);
+           canvas.drawRect(recWidth+30, recHight/4 + 100  ,  (recWidth+30)*2, recHight, myPaint);
+           canvas.drawRect((recWidth+30)*2,100 ,  (recWidth+30)*3, recHight, myPaint);
+            canvas.drawRect((recWidth+30)*3, recHight/4 + 100 ,  (recWidth+30)*4, recHight, myPaint);
+            //canvas.drawRect(20, recHight - (recHight/4)*2 ,  recWidth/4, 200, myPaint);
+            //canvas.drawRect(20, recHight - (recHight/4)*2 ,  recWidth/4, 200, myPaint);
+
+            Log.d("hight",String.valueOf(recHight));
+            Log.d("hight",String.valueOf(recWidth));
+
+            holder.unlockCanvasAndPost(canvas);
+        }
+
     }
 }
